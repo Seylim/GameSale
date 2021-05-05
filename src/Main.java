87e7e47997d4e543a1e0ae9b9c43ff8343ecfc22
Emre Kaan Seylim <seylimemre@gmail.com>
2021-05-05 17:52:45 +0300

    Day_4_Homework_3

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
 	public static void main(String[] args) throws RemoteException {
 		
-		User user = new User(1, "Emre Kaan", "Seylim", "12345123451", 2000);
+		User user = new User(1, "Emre Kaan", "Seylim", "12345123465", 2000);
 		
 		UserCheckService userCheckService = new MernisServiceAdapter();
 		UserManager userManager = new UserManager(userCheckService);
